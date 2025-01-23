73adcac7efcfe9694912a3ff512c76836ae134d9
dev4all12 <dev4all12@gmail.com>
2025-01-23 17:44:28 +0530

    User data called from  User details;

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:api_work/user/user.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
@@ -11,7 +12,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<dynamic> users = [];
+  List<User> users = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,22 +23,22 @@ class _HomeScreenState extends State<HomeScreen> {
           itemCount: users.length,
           itemBuilder: (context, index) {
             final user = users[index];
-            final name = user['name']['first'];
-            final email = user['email'];
-            final imgUrl = user['picture']['thumbnail'];
+            // final name = user['name']['first'];
+            final email = user.email;
+            // final imgUrl = user['picture']['thumbnail'];
             return ListTile(
-              leading: ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: Image.network(imgUrl),
-              ),
+              // leading: ClipRRect(
+              //   borderRadius: BorderRadius.circular(100),
+              //   child: Image.network(imgUrl),
+              // ),
 
               // leading: CircleAvatar(
               //   child: Image.network(imgUrl),
 
               //   // Text('${index + 1}'),
               // ),
-              title: Text(name),
-              subtitle: Text(email),
+              title: Text(email),
+              // subtitle: Text(email),
             );
           }),
       floatingActionButton: FloatingActionButton(
